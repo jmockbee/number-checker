6f7d8cc921b70723d73c1aa13c6fdf8e0dfd6a3f
Jon Mockbee <mockbee1000@gmail.com>
2019-07-15 15:53:00 -0400

    added the check in main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+
+    public static void main(String[] args) {
+        boolean checkingteen =  TeenNumberChecker.hasTeen(12,14,24);
+        boolean againcheckingteen = TeenNumberChecker.isTeen(8);
+        System.out.println("The first boolean is hasTeen the second boolean isTeen = "+checkingteen+ " " +againcheckingteen);
+
+    }
 }
